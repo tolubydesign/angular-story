f70e3f2f95c4e74de5c689d4f6172b6f6509362e
Tolu Adesina <tolu.adesina79@gmail.com>
2021-05-17 23:58:26 +0200

    UPDATE angular core and cli from version 10 to 11

--- src/app/character-portrait/character-portrait.component.spec.ts
+++ src/app/character-portrait/character-portrait.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CharacterPortraitComponent } from './character-portrait.component';
 
@@ -6,7 +6,7 @@ describe('CharacterPortraitComponent', () => {
   let component: CharacterPortraitComponent;
   let fixture: ComponentFixture<CharacterPortraitComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CharacterPortraitComponent ]
     })

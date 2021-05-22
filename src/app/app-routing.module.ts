71f225467e3c83cd5175ab62c8b5a0f5c3d5c8f6
Tolu Adesina <tolu.adesina79@gmail.com>
2021-05-22 18:16:48 +0200

    ADD component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/** COMPONENTS */
+import { StartScreenComponent } from './shared/components/start-screen/start-screen.component';
+import { EditorComponent } from './shared/components/editor/editor.component';
+import { InteractionComponent } from './shared/components/interaction/interaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartScreenComponent },
+  { path: 'editor', component: EditorComponent },
+  { path: 'interaction', component: InteractionComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class AppRoutingModule { }

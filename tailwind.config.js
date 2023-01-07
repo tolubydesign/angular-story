39488ccbe8d2a40ba641fbc3a753271c9b88e8e7
Tolu Adesina <tolu.adesina79@gmail.com>
2023-01-07 10:46:39 +0200

    REFACTOR tailwind config, support just-in-time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  corePlugins: {
-    preflight: false,
-  },
+  media: false,
   content: [
-    "./src/**/*.{html,ts}",
+    './public/**/*.html',
+    "./src/**/*.{ts,html,scss}"
   ],
+  // content: [
+  //   "./src/**/*.{html,ts}",
+  // ],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Roboto-Mono', 'Helvetica Neue', 'sans-serif'],
-      serif: ['Roboto', 'serif'],
-    },
+    // fontFamily: {
+    //   sans: ['Roboto-Mono', 'Helvetica Neue', 'sans-serif'],
+    //   serif: ['Roboto', 'serif'],
+    // },
     extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
+      colors: {
+        green: require('tailwindcss/colors').emerald,
+        yellow: require('tailwindcss/colors').amber,
+        purple: require('tailwindcss/colors').violet,
       }
     }
   },

83c8306abdb728b6265d80632f5c06414768e3cc
Tolu Adesina <tolu.adesina79@gmail.com>
2024-02-06 22:29:29 +0200

    ADD development environment variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 import { Environment } from "./environment.model";
 
 export const environment: Environment = {
-  production: true,
-  baseUrl: "https://xvcu0wf8hf.execute-api.us-east-2.amazonaws.com/dev",
+  production: false,
+  baseUrl: "http://127.0.0.1:2100",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
+import { Environment } from "./environment.model";
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environment: Environment = {
   production: false,
+  baseUrl: "about:blank"
 };
 
 /*

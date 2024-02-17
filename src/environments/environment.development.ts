7be610cc52420c02874844aa1d323749b79f7c05
Tolu Adesina <tolu.adesina79@gmail.com>
2024-02-17 16:36:04 +0200

    REMOVE development and prod env ts file

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from "./environment.model";
-
-export const environment: Environment = {
-  production: false,
-  baseUrl: "http://127.0.0.1:2100",
-};
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from "./environment.model";
-
-export const environment: Environment = {
-  production: true,
-  baseUrl: "https://xvcu0wf8hf.execute-api.us-east-2.amazonaws.com/dev",
-};

e1e6c3cdd56aca3d5d4ab5ed42a2b6891502cf3c
Tolu Adesina <tolu.adesina79@gmail.com>
2024-02-28 22:46:04 +0200

    ADD basic navigation structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from '@angular/core';
-
-/* angular material */
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
@@ -11,34 +9,22 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-/* router */
 import { AppRoutingModule } from "./app-routing.module";
-
-/* requests */
 import { HttpClientModule } from "@angular/common/http";
-
-/* graph component */
-// import { NgxEchartsModule } from 'ngx-echarts';
-
-/* form */
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-/* components */
 import { AppComponent } from "./app.component";
 import { DashboardComponent } from "./shared/components/interaction-mode/dashboard/dashboard.component";
 import { ButtonComponent } from "./shared/components/ui/button/button.component";
 import { NavButtonComponent } from "./shared/components/ui/nav-button/nav-button.component";
 import { CharacterPortraitComponent } from "./shared/components/ui/character-portrait/character-portrait.component";
 import { NarrativeComponent } from "./shared/components/interaction-mode/narrative/narrative.component";
-import { StartScreenComponent } from "./shared/components/pages/start-screen/start-screen.component";
 import { InteractionComponent } from "./shared/components/interaction-mode/interaction/interaction.component";
 import { EditorComponent } from "./shared/components/editor-mode/editor/editor.component";
 import { PanelComponent } from "./shared/components/extra/panel/panel.component";
 import { DagreComponent } from "./shared/components/extra/d3-components/dagre/dagre.component";
 import { SankeyComponent } from "./shared/components/extra/d3-components/sankey/sankey.component";
 import { DendrogramComponent } from "./shared/components/extra/d3-components/dendrogram/dendrogram.component";
-import { NotFoundComponent } from './shared/components/pages/not-found/not-found.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DashboardPanelComponent } from './shared/components/extra/dashboard-panel/dashboard-panel.component';
 import { LoaderComponent } from './shared/components/ui/loader/loader.component';
 import { HierarchyComponent } from './shared/components/extra/hierarchy/hierarchy.component';
@@ -48,17 +34,16 @@ import { StoryBoardComponent } from './shared/components/interaction-mode/story-
 import { EditingComponent } from './shared/components/editor-mode/editing/editing.component';
 import { NodeFormComponent } from './shared/components/editor-mode/node-form/node-form.component';
 import { SnackBarNotificationComponent } from './core/snack-bar-notification/snack-bar-notification.component';
+import { NavigationComponent } from "./core/navigation/navigation.component";
 
 @NgModule({
   declarations: [
-    // Components
     AppComponent,
     DashboardComponent,
     ButtonComponent,
     NavButtonComponent,
     CharacterPortraitComponent,
     NarrativeComponent,
-    StartScreenComponent,
     InteractionComponent,
     EditorComponent,
     PanelComponent,
@@ -94,6 +79,7 @@ import { SnackBarNotificationComponent } from './core/snack-bar-notification/sna
     MatSnackBarModule,
     MatInputModule,
     MatFormFieldModule,
+    NavigationComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/core/navigation/navigation.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+
+  navigationLinkClassName = 'cursor-pointer mb-2 rounded-full hover:bg-[#fff]';
+
+
+
+}

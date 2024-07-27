f2cac07f269252be99daea643787c6ecef0f4752
Tolu Adesina <tolu.adesina79@gmail.com>
2024-07-27 15:29:08 +0200

    UPDATE user registration

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -1,12 +1,19 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { environment } from '@environment/environment';
-import { BehaviorSubject, catchError, finalize, tap } from "rxjs";
+import { BehaviorSubject, catchError, finalize, Observable, tap } from "rxjs";
 import { NotificationService } from "@services/notification.service";
 import { handleServiceError } from "@shared/utils/error-notification-handler";
 import { UserCredentials } from "@models/user.models";
 import { setUserCredential, getUserCredentials } from "@helpers/session.storage";
 import { HTTPSuccessResponse } from "@shared/models/http.model";
+
+type UserRegisterInfo = {
+  email: string,
+  password: string,
+  forename: string,
+  surname: string
+}
 
 @Injectable({
   providedIn: 'root'
@@ -27,7 +34,7 @@ export class UserService {
 
   private handleError = handleServiceError
 
-  login({ email, password }: { email: string, password: string }) {
+  login({ email, password }: { email: string, password: string }): Observable<HTTPSuccessResponse<UserCredentials> | HttpErrorResponse> {
     const url = `${this._url}/login`;
     return this.http.post<UserCredentials>(url, { email, password })
       .pipe(catchError(this.handleError(this.notificationService)))
@@ -38,6 +45,34 @@ export class UserService {
 
       .pipe(tap((response: HTTPSuccessResponse<UserCredentials>) => {
         console.log('login request response', response);
+        setUserCredential({
+          username: response.data.username,
+          token: response.data.token,
+          email: response.data.email,
+          role: response.data.role,
+        })
+        return response
+      }));
+  }
+
+  register({email, password, forename, surname }: UserRegisterInfo): Observable<HTTPSuccessResponse<UserCredentials> | HttpErrorResponse> {
+    const url = `${this._url}/register`;
+
+    return this.http.post<UserCredentials>(url, { 
+      email, 
+      password, 
+      "name": forename,
+      "surname": surname,
+      "username": ""
+    })
+      .pipe(catchError(this.handleError(this.notificationService)))
+
+      .pipe(finalize(() => {
+        console.log("User Registration request finished");
+      }))
+
+      .pipe(tap((response: HTTPSuccessResponse<UserCredentials>) => {
+        console.log('register request response', response);
         setUserCredential({
           username: response.data.username,
           token: response.data.token,
--- src/app/shared/utils/validation.ts
+++ src/app/shared/utils/validation.ts
@@ -1,4 +1,4 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { AbstractControl, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
 
 /** */
 export function forbiddenPasswordValidator(nameRe: RegExp): ValidatorFn {
@@ -16,5 +16,64 @@ export function emailValidation(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const forbidden = !emailRegex.test(control.value);
     return forbidden ? { forbiddenName: { value: control.value } } : null;
+  };
+}
+
+export function passwordPattern(): ValidatorFn {
+  return Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/)
+}
+
+/**
+ * Generic Form Group input matching field.
+ * ...
+ * @param controlName 
+ * @param matchingControlName 
+ * @param errorMessage 
+ * @returns 
+ */
+export function inputFieldsMatch(controlName: string, matchingControlName: string, errorMessage: string): ValidatorFn {
+  return (abstractControl: AbstractControl) => {
+    const control = abstractControl.get(controlName);
+    const matchingControl = abstractControl.get(matchingControlName);
+
+    if (matchingControl!.errors && !matchingControl!.errors?.['matchingFieldsValidator']) {
+      return null;
+    }
+
+    if (control!.value !== matchingControl!.value) {
+      const error = { matchingFieldsValidator: errorMessage };
+      matchingControl!.setErrors(error);
+      return error;
+    } else {
+      matchingControl!.setErrors(null);
+      return null;
+    }
+  };
+}
+
+/**
+ * Password matching Form Group Validator.
+ * ... 
+ * @param controlName 
+ * @param matchingControlName 
+ * @returns 
+ */
+export function passwordMatch(controlName: string, matchingControlName: string): ValidatorFn {
+  return (abstractControl: AbstractControl) => {
+    const control = abstractControl.get(controlName);
+    const matchingControl = abstractControl.get(matchingControlName);
+
+    if (matchingControl!.errors && !matchingControl!.errors?.['confirmedValidator']) {
+      return null;
+    }
+
+    if (control!.value !== matchingControl!.value) {
+      const error = { confirmedValidator: 'Passwords do not match.' };
+      matchingControl!.setErrors(error);
+      return error;
+    } else {
+      matchingControl!.setErrors(null);
+      return null;
+    }
   };
 }
